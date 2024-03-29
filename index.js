844b559ea7d3e029b04426818df8e2c4151f7417
Stacy <salyiela@icloud.com>
2024-03-29 10:30:13 +0300

    Completed arrow function lab

---
+++ index.js
@@ -0,0 +1,14 @@
+// to define function divide
+const divide = (nums, nums2) => {
+    nums = 2000;
+    nums2 = 100;
+    return nums / nums2;
+};
+
+// to define function square and perform square root
+const square = (num) => num * num;
+square(5);
+
+// to use arrow function to add two number
+const add = (num1, num2) => num1 + num2;
+add(6,6);
